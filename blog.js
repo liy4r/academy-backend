1336d95428c7d8dcf4eec8480eec94fbe9f878ea
Dulguun Batmunkh <b.dulguun0114@gmail.com>
2025-10-27 15:28:57 +0800

    blogs duusgav

--- blog.js
+++ blog.js
@@ -3,8 +3,14 @@ const b = document.getElementById("content");
 const c = document.getElementById("addBtn");
 const d = document.getElementById("posts");
 const post = () => {
-    const e = document.body.appendChild(document.createElement("h3"))
-        const f = document.body.appendChild(document.createElement("p"))
+d.innerHTML=" ";
+const listA=document.createElement("li");
+d.appendChild(listA);
+const e = document.createElement("h3");
+listA.appendChild(e);
+const f = document.createElement("p");
+d.appendChild(f);
              e.innerText=a.value;
+                f.innerText=b.value;
 }
 c.addEventListener("click", post);
